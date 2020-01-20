a132083ca5f736b86aa1cf8fc799dc0d1f7e6323
Michael Levi <michaellevi151@gmail.com>
2020-01-20 18:23:09 -0500

    Use H2 for db

--- src/main/kotlin/com/whatsappgrouper/spring/Application.kt
+++ src/main/kotlin/com/whatsappgrouper/spring/Application.kt
@@ -1,21 +1,34 @@
 package com.whatsappgrouper.spring
 
+import com.whatsappgrouper.spring.repository.MessageRepository
 import com.whatsappgrouper.spring.repository.api.WhatsappService
+import com.whatsappgrouper.spring.repository.db.MessageDatabase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
 import okhttp3.OkHttpClient
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.event.EventListener
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import kotlin.math.log
+
 
 @SpringBootApplication
 @EntityScan(basePackages = ["com.whatsappgrouper.spring.model"])
 @EnableJpaRepositories(basePackages = ["com.whatsappgrouper.spring.model", "com.whatsappgrouper.spring.repository.db"])
 class Application {
+
+    val logger = KotlinLogging.logger{}
 
     @Value("\${com.whatsappgrouper.api-token}")
     lateinit var apiToken: String
@@ -52,6 +65,18 @@ class Application {
     fun whatsappService(retrofit: Retrofit): WhatsappService {
         val whatsappService = retrofit.create(WhatsappService::class.java)
         return whatsappService
+    }
+
+    @Bean
+    fun runner(whatsappService: WhatsappService, messageDatabase: MessageDatabase) = CommandLineRunner {
+        runBlocking {
+            GlobalScope.launch {
+                val messages = whatsappService.getMessages()
+                logger.info { messages }
+                messageDatabase.saveAll(messages.messages)
+                logger.info { messageDatabase.findAll() }
+            }
+        }
     }
 }
 
--- src/main/kotlin/com/whatsappgrouper/spring/repository/MessageRepository.kt
+++ src/main/kotlin/com/whatsappgrouper/spring/repository/MessageRepository.kt
@@ -1,5 +1,6 @@
 package com.whatsappgrouper.spring.repository
 
+import com.whatsappgrouper.spring.model.Message
 import com.whatsappgrouper.spring.repository.api.WhatsappService
 import com.whatsappgrouper.spring.repository.db.MessageDatabase
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,6 +12,14 @@ class MessageRepository {
     @Autowired lateinit var messageDatabase: MessageDatabase
     @Autowired lateinit var whatsappService: WhatsappService
 
+    fun save(message: Message) {
+        messageDatabase.save(message)
+    }
 
+    suspend fun sendMessage(chatId: String, body: String) {
+        val sentMessageResponse = whatsappService.sendMessageToChatId(chatId, body)
+
+        TODO("retrieve message from server and save it")
+    }
 
 }
